15f1af6c7619e86c1c7eb4ca8439462130ec8a98
Loren Yu <loren@navapbc.com>
2024-09-26 15:21:01 -0700

    Move commands

---
+++ nava/commands/infra/__init__.py
@@ -0,0 +1,25 @@
+import click
+
+
+@click.group()
+def infra():
+    pass
+
+
+@infra.command()
+@click.argument("template_dir")
+@click.argument("project_dir")
+def install(template_dir, project_dir):
+    click.echo(
+        f"Installing infrastructure from template {template_dir} to project {project_dir}..."
+    )
+
+
+@infra.command()
+def add_app():
+    click.echo("Adding application to infrastructure...")
+
+
+@infra.command()
+def update():
+    click.echo("Updating infrastructure...")
