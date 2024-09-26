11ebf1f925119a53baaf4470ac8c3fc3b915cb37
Loren Yu <loren@navapbc.com>
2024-09-26 15:02:25 -0700

    Add type hints

--- tests/commands/infra/test_get_app_names.py
+++ tests/commands/infra/test_get_app_names.py
@@ -1,10 +1,12 @@
 import pytest
+from pathlib import Path
+from typing import List
 
 from nava.commands.infra.get_app_names import get_app_names
 
 
 @pytest.fixture
-def tmp_template(tmp_path):
+def tmp_template(tmp_path: Path) -> Path:
     infra_dir = tmp_path / "infra"
     infra_dir.mkdir()
     (infra_dir / "app1").mkdir()
@@ -16,7 +18,7 @@ def tmp_template(tmp_path):
     return tmp_path
 
 
-def test_get_app_names(tmp_template):
+def test_get_app_names(tmp_template: Path) -> None:
     # Call the function with the temporary directory
-    app_names = get_app_names(tmp_template)
+    app_names: List[str] = get_app_names(str(tmp_template))
     assert set(app_names) == set(["app1", "app2"])
