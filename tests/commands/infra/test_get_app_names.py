380aeaf0f20f75fe70cd77c5b77e46a694e9a57c
Loren Yu <loren@navapbc.com>
2024-09-27 16:21:00 -0700

    Fix get app names

--- tests/commands/infra/test_get_app_names.py
+++ tests/commands/infra/test_get_app_names.py
@@ -2,8 +2,23 @@ from pathlib import Path
 from typing import List
 
 from nava.commands.infra.get_app_names import get_app_names
+from tests.lib import DirectoryContent
 
 
-def test_get_app_names(tmp_template: Path) -> None:
-    app_names: List[str] = get_app_names(str(tmp_template))
+def test_get_app_names(tmp_path: Path) -> None:
+    DirectoryContent(
+        {
+            "infra": {
+                "accounts": {},
+                "app1": {},
+                "app2": {},
+                "modules": {
+                    "service": {},
+                },
+                "networks": {},
+                "project-config": {},
+            }
+        }
+    ).to_fs(str(tmp_path))
+    app_names = get_app_names(str(tmp_path))
     assert set(app_names) == set(["app1", "app2"])
