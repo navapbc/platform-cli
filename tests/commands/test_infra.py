9dc0190e7160d67654a642487b5ceca14819ef1c
Loren Yu <loren@navapbc.com>
2024-09-26 17:09:17 -0700

    Remove unused var

--- tests/commands/test_infra.py
+++ tests/commands/test_infra.py
@@ -2,5 +2,4 @@ from click.testing import CliRunner
 
 
 def test_install(cli, tmp_template, tmp_project):
-    runner = CliRunner()
     cli(["infra", "install", str(tmp_template), str(tmp_project)])
