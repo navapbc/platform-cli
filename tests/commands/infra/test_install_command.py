98a3c852f719d90e6b4492ae390eb9d4dece90a2
Loren Yu <loren@navapbc.com>
2024-10-01 15:22:59 -0700

    Separate install and update tests

--- tests/commands/infra/test_install_command.py
+++ tests/commands/infra/test_install_command.py
@@ -1,4 +1,3 @@
-import traceback
 from tests.lib import DirectoryContent, FileChange, RenameChange, git
 
 # Remaining test cases to implement
@@ -62,30 +61,3 @@ def test_install(cli, tmp_template, tmp_project):
     short_hash = template_commit_hash[:7]
     assert short_hash in dir_content[".template"][".template-infra-app-foo.yml"]
     assert short_hash in dir_content[".template"][".template-infra-base.yml"]
-
-
-def test_update_no_change(cli, tmp_template, tmp_project, clean_install):
-    content_before_update = DirectoryContent.from_fs(tmp_project, ignore=[".git"])
-
-    cli(["infra", "update", str(tmp_template), str(tmp_project)])
-
-    content_after_update = DirectoryContent.from_fs(tmp_project, ignore=[".git"])
-    assert content_before_update == content_after_update
-
-
-# def test_update_with_change(cli, tmp_template, tmp_project, clean_install):
-#     content_before_update = DirectoryContent.from_fs(tmp_project, ignore=[".git"])
-
-#     FileChange("infra/modules/service/main.tf", "", "changed").apply(tmp_template)
-
-#     cli(["infra", "update", str(tmp_template), str(tmp_project)])
-
-#     assert (
-#         git.diff(tmp_project)
-#         == """diff --git a/tests/commands/test_infra.py b/tests/commands/test_infra.py
-# index 991c4e3..e788003 100644
-# --- a/tests/commands/test_infra.py
-# +++ b/tests/commands/test_infra.py
-# +        changed
-# """
-#     )
