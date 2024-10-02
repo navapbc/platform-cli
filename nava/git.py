1f47f5f2b564190284aec51281da486cfa4303ea
Loren Yu <loren@navapbc.com>
2024-10-01 21:50:58 -0700

    Move git methods to class

--- nava/git.py
+++ nava/git.py
@@ -2,6 +2,30 @@ from pathlib import Path
 import subprocess
 
 
+class GitProject:
+
+    def __init__(self, dir: Path):
+        self.dir = Path(dir)
+
+    def init(self) -> None:
+        subprocess.run(["git", "init"], cwd=self.dir)
+
+    def commit(self, msg: str) -> None:
+        subprocess.run(["git", "add", "."], cwd=self.dir)
+        subprocess.run(["git", "commit", "-m", msg], cwd=self.dir)
+
+    def commit_hash(self) -> str:
+        return subprocess.run(
+            ["git", "rev-parse", "HEAD"], cwd=self.dir, capture_output=True, text=True
+        ).stdout.strip()
+
+    def stash(self) -> None:
+        subprocess.run(["git", "stash"], cwd=self.dir)
+
+    def pop(self) -> None:
+        subprocess.run(["git", "stash", "pop"], cwd=self.dir)
+
+
 def init(dir: str | Path) -> None:
     subprocess.run(["git", "init"], cwd=dir)
 
--- nava/infra_template.py
+++ nava/infra_template.py
@@ -40,6 +40,8 @@ class InfraTemplate:
     def update(self, project: Project):
         num_changes = 0
 
+        git_project = git.GitProject(project.project_dir)
+
         copier.run_update(
             project.project_dir,
             answers_file=self._base_answers_file_path(),
@@ -47,7 +49,7 @@ class InfraTemplate:
             overwrite=True,
             skip_answered=True,
         )
-        git.stash(project.project_dir)
+        git_project.stash()
         num_changes += 1
 
         for app_name in project.app_names:
@@ -58,11 +60,11 @@ class InfraTemplate:
                 overwrite=True,
                 skip_answered=True,
             )
-            git.stash(project.project_dir)
+            git_project.stash()
             num_changes += 1
 
         for i in range(num_changes):
-            git.pop(project.project_dir)
+            git_project.pop()
 
     def add_app(self, project: Project, app_name: str):
         data = {"app_name": app_name}
