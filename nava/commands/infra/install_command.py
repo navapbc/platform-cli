45e235c8caa7cc37b4935d8ee9156c30227d48f4
Loren Yu <loren@navapbc.com>
2024-10-01 21:54:06 -0700

    Unused imports

--- nava/commands/infra/install_command.py
+++ nava/commands/infra/install_command.py
@@ -1,15 +1,9 @@
 from pathlib import Path
 import click
 
-
 from nava.infra_template import InfraTemplate
 from nava.project import Project
 from .get_app_names import get_app_names
-from . import add_app_command
-
-from .compute_app_includes_excludes import (
-    compute_app_includes_excludes,
-)
 
 
 def install(template_dir: str, project_dir: str):
