5ca36ed731c49f78abaecb154e4f0f725e5854aa
Tanner Doshier <git@doshitan.com>
2024-11-05 17:28:48 -0500

    Use data arg for app name during install if available

--- nava/platform/cli/commands/infra/install_command.py
+++ nava/platform/cli/commands/infra/install_command.py
@@ -15,7 +15,12 @@ def install(
     template = InfraTemplate(Path(template_dir))
     project = Project(Path(project_dir))
     app_names = project.app_names
+
     if len(app_names) == 0:
-        app_name = click.prompt("What is the name of your app?", type=str)
-        app_names = [app_name]
+        if data and "app_name" in data:
+            app_names = [data["app_name"]]
+        else:
+            app_name = click.prompt("What is the name of your app?", type=str)
+            app_names = [app_name]
+
     template.install(project, app_names, version=version, data=data)
