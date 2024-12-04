2f17930b47f0051fe17fd1daf9d80bed92be5d49
Tanner Doshier <git@doshitan.com>
2024-12-04 12:17:51 -0500

    WIP update existing app

--- nava/platform/project.py
+++ nava/platform/project.py
@@ -17,4 +17,14 @@ class Project:
     def installed_template_names(self) -> Iterable[str]:
         state_dirs = filter(lambda f: f.is_dir(), self.dir.glob(".template-*"))
 
-        return map(lambda d: d.name, state_dirs)
+        return map(lambda d: d.name.removeprefix("."), state_dirs)
+
+    def installed_template_names_for_app(self, app_name: str) -> Iterable[str]:
+        answer_files = self._installed_template_answer_files_for_app(app_name)
+
+        return map(lambda f: f.parent.name.removeprefix("."), answer_files)
+
+    def _installed_template_answer_files_for_app(self, app_name: str) -> Iterable[Path]:
+        answer_files = filter(lambda f: f.is_file(), self.dir.glob(f".template-*/*{app_name}*"))
+
+        return answer_files
