4b99f5f9cd66e94a46f7af91fea8e8c3bdf967ed
Tanner Doshier <git@doshitan.com>
2025-07-18 10:54:46 -0400

    Package updates

--- nava/platform/util/wrappers.py
+++ nava/platform/util/wrappers.py
@@ -9,7 +9,7 @@ R = TypeVar("R")
 def print_call(func: Callable[P, R], printer: Callable[[str], None] = print) -> Callable[P, R]:
     return wrap_call(
         func,
-        lambda args, kwargs: printer(
+        lambda *args, **kwargs: printer(
             f"Calling: {func.__name__} with args:\n{args}\n and kwargs:\n{kwargs}"
         ),
     )
@@ -18,16 +18,16 @@ def print_call(func: Callable[P, R], printer: Callable[[str], None] = print) -> 
 def log_call(func: Callable[P, R], logger: Callable[..., None]) -> Callable[P, R]:
     return wrap_call(
         func,
-        lambda args, kwargs: logger(
+        lambda *args, **kwargs: logger(
             "Calling function", name=func.__name__, args=args, kwargs=kwargs
         ),
     )
 
 
-def wrap_call(func: Callable[P, R], do: Callable[[P.args, P.kwargs], None]) -> Callable[P, R]:
+def wrap_call(func: Callable[P, R], do: Callable[P, None]) -> Callable[P, R]:
     @functools.wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-        do(args, kwargs)
+        do(*args, **kwargs)
         return func(*args, **kwargs)
 
     return wrapper
